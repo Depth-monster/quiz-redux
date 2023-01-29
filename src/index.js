252d21f92e52f561cd6ec3a294352fe05843df82
Azat <john.mayson7122000@gmail.com>
2023-01-29 21:15:57 +0300

    uploading

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Quiz from './components/Quiz';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Quiz />
+  </React.StrictMode>
+);
+
