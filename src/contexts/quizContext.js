01ac886f42eeff8f68095579d6f809abba2e8067
Azat <john.mayson7122000@gmail.com>
2023-01-31 00:23:09 +0300

    checing email

--- src/contexts/quizContext.js
+++ src/contexts/quizContext.js
@@ -1,27 +1,25 @@
 import { createContext, useReducer } from "react";
-import { act } from "react-dom/test-utils";
 import questions from "../data";
-
-export const QuizContext = createContext();
-
-const reducer = (state, action ) => {
-console.log(action)
-switch (action.type){
-    case "NEXT_QUESTION":{
-        return{...state,
-        currentQuestionIndex:state.currentQuestionIndex+1}
-    };
-
-}
-  return state;
-};
 
 const initialState = {
   questions,
-  currentQuestionIndex:0
+  currentQuestionIndex: 0,
 };
 
+const reducer = (state, action) => {
+ // console.log(action);
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "NEXT_QUESTION": {
+      return { ...state, currentQuestionIndex: state.currentQuestionIndex + 1 };
+    }
+  }
+  return state;
+};
+
+export const QuizContext = createContext();
+
 export const QuizProvider = ({ children }) => {
-  const value = useReducer(reducer, initialState);//через useReducer получаем доступ к state
-  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;//через провайдер передаем 
+  const value = useReducer(reducer, initialState); //через useReducer получаем доступ к state
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>; //через провайдер передаем
 };
